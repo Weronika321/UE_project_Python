73cc0c42e620cf0c08cd74d67117c2eb43e039a1
Weronika Ładak <weronika.ladak1@gmail.com>
2022-10-09 10:35:16 +0200

    number and image

--- invert_colors.py
+++ invert_colors.py
@@ -1,11 +1,6 @@
 from PIL import Image, ImageChops
 
 def invert_colors(filename):
-    obrazek = Image.open(filename)
-    obrazek_inw = ImageChops.invert(obrazek)
-    return obrazek_inw
-    # obrazek_inw.show()
-    
-
-#obrazek = Image.open("obrazek.jpg")
-#inwersja_kolorow(obrazek)
+    img = Image.open(filename)
+    result = ImageChops.invert(img)
+    return result
--- main.py
+++ main.py
@@ -1,43 +1,28 @@
-from http.client import responses
-from random import randint
-from PIL import Image, ImageChops
-
 import os
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi.responses import FileResponse
-
-from is_prime import is_prime
+from fastapi.responses import FileResponse, HTMLResponse
+from primality_test import primality_test
 from invert_colors import invert_colors
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return "Hello world!"
-
-
-@app.get("/prime/{number}")
+@app.get("/prime/{number}", response_class=HTMLResponse)
 def check_number(number):
-    return is_prime(number)
-
-@app.get("/invert")
-def check_number():
-    picture = Image.open("pencils.jpg")
-    # picture.show()
-    return "obrazek"
-
-    # return self.value is int and self.value > 1 and self.value < 9223372036854775807
-
+    html_content = f"""
+    <html>
+        <body>
+            <h1>{primality_test(number)}</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+    
 
 @app.get("/picture/{filename}", response_class=FileResponse)
-async def read_items(filename):
+def invert_image(filename):
     img = invert_colors(os.path.abspath(filename))
     img.save("image.jpg")
     return os.path.abspath("image.jpg")
-    
-# img = invert_colors(os.path.abspath("pobrane.jpg"))
-# img.show()
-# img = invert_colors(os.path.abspath("pobrane.jpg"))
-# print(img.path)
+
+
