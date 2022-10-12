d6c99a41ca0dd5a082b93dbdd822c962d55dd5b0
Weronika Ładak <weronika.ladak1@gmail.com>
2022-10-12 12:11:28 +0200

    authentication

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.responses import FileResponse, HTMLResponse
+import os
+import datetime
 from primality_test import primality_test
 from invert_colors import invert_colors
+from authentication import get_current_username
+
 
 app = FastAPI()
 
@@ -26,3 +29,14 @@ def invert_image(filename):
     return os.path.abspath("image.jpg")
 
 
+@app.get("/auth")
+def read_current_user(username: str = Depends(get_current_username)):
+    time = datetime.datetime.now().strftime("%H:%M")
+    html_content = f"""
+    <html>
+        <body>
+            <h1>Aktualna godzina: {time}</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
