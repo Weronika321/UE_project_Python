ace71e0e39e62d9669e9065669cffc824e8a76c9
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 10:23:20 +0100

    corrected

--- api/InvertColors.py
+++ api/InvertColors.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 
 
-class HelloWorld(Resource):
+class InvertColors(Resource):
     def get(self):
-        return {"hello": "world"}
+        return {"hello": "color"}
--- api/authentication.py
+++ api/authentication.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 
 
-class HelloWorld(Resource):
+class Authentication(Resource):
     def get(self):
-        return {"hello": "world"}
+        return {"hello": "aut"}
