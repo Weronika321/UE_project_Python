b6589227d07dd36bd6c6812d963f85ce7e0c70a7
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 10:09:12 +0100

    number

--- api/primality_test.py
+++ api/primality_test.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
-from app.primality_test import primality_test
+# from app.primality_test import primality_test
 
 class PrimalityTest(Resource):
     def get(self):
         number = 9
-        return primality_test(number)
+        return number  #primality_test(number)
