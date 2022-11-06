18e201732f6bae15be33db6de7d9f1c44f5a07f1
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 12:33:17 +0100

    copy

--- main.py
+++ main.py
@@ -1,37 +1,125 @@
-from flask import Flask
-from flask_restful import Api
-from markupsafe import escape
-from flask import Flask, render_template, redirect, url_for, request
+# from flask import Flask
+# from flask_restful import Api
+# from markupsafe import escape
+# from flask import Flask, render_template, redirect, url_for, request
 
 
 
-from app.primality_test import primality_test
+# from app.primality_test import primality_test
 
+
+# app = Flask(__name__)
+# api = Api(app)
+
+# @app.get('/')
+# def start(): # https://project-ue.herokuapp.com/
+#     return ''
+    
+# @app.get('/prime/<number>')
+# def check_number(number): # https://project-ue.herokuapp.com/prime/3
+#     return f'''<h1>{primality_test(escape(number))}</h1>'''
+    
+# @app.get('/time')
+# def show_time(): # https://project-ue.herokuapp.com/time
+#     return "time"
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+#             error = 'Invalid Credentials. Please try again.'
+#         else:
+#             return redirect(url_for('start'))
+#     return render_template('login.html', error=error)
+
+# if __name__ == "__main__":
+#     app.run(debug=False, host="0.0.0.0")
+
+
+from flask import Flask, Response, redirect, url_for, request, session, abort
+from flask_login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user 
 
 app = Flask(__name__)
-api = Api(app)
 
-@app.get('/')
-def start(): # https://project-ue.herokuapp.com/
-    return ''
-    
-@app.get('/prime/<number>')
-def check_number(number): # https://project-ue.herokuapp.com/prime/3
-    return f'''<h1>{primality_test(escape(number))}</h1>'''
-    
-@app.get('/time')
-def show_time(): # https://project-ue.herokuapp.com/time
-    return "time"
+# config
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
 
-@app.route('/login', methods=['GET', 'POST'])
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+# create some users with ids 1 to 20       
+users = [User(id) for id in range(1, 21)]
+
+
+# some protected url
+@app.route('/')
+@login_required
+def home():
+    return Response("Hello World!")
+
+ 
+# somewhere to login
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        username = request.form['username']
+        password = request.form['password']        
+        if password == username + "_secret":
+            id = username.split('user')[1]
+            user = User(id)
+            login_user(user)
+            return redirect(request.args.get("next"))
         else:
-            return redirect(url_for('start'))
-    return render_template('login.html', error=error)
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+
+# somewhere to logout
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return Response('<p>Logged out</p>')
+
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+    
+    
+# callback to reload the user object        
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+    
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
+    app.run()
