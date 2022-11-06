56177a2306082b2b81a9e8e7e836483ce2419a4d
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 11:54:54 +0100

    login

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 # from markupsafe import escape
+from flask import Flask, render_template, redirect, url_for, request
+
 
 
 from app.primality_test import primality_test
@@ -21,6 +23,16 @@ def access_param(): # https://project-ue.herokuapp.com/
 def access_param(): # https://project-ue.herokuapp.com/time
     return f"time"
 
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0")
