41035508b67ab5f21b9eeb3647df31ecfdd6e09a
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 12:46:24 +0100

    .

--- main.py
+++ main.py
@@ -23,15 +23,28 @@ def check_number(number): # https://project-ue.herokuapp.com/prime/3
 def show_time(): # https://project-ue.herokuapp.com/time
     return "time"
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    # if request.method == 'POST':
-    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-    #         error = 'Invalid Credentials. Please try again.'
-    #     else:
-    #         return redirect(url_for('start'))
-    return render_template('app/login.html', error=error)
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.current_user())
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0")
