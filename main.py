7869b45d90fb3ae70f56b97ce95036372eae7d10
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 12:00:33 +0100

    com

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 # from markupsafe import escape
-from flask import Flask, render_template, redirect, url_for, request
+# from flask import Flask, render_template, redirect, url_for, request
 
 
 
@@ -24,15 +24,15 @@ def access_param(): # https://project-ue.herokuapp.com/time
     return f"time"
 
 # Route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+#             error = 'Invalid Credentials. Please try again.'
+#         else:
+#             return redirect(url_for('home'))
+#     return render_template('login.html', error=error)
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0")
