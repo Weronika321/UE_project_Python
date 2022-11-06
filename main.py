7eb5a9c5f3ef67ada12c3195650c3f56417a6aa8
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 12:53:39 +0100

    .

--- main.py
+++ main.py
@@ -1,77 +1,50 @@
-# from flask import Flask
-# from flask_restful import Api
-# from markupsafe import escape
-# from flask import Flask, render_template, redirect, url_for, request
-
-
-
-# from app.primality_test import primality_test
-
-
-# app = Flask(__name__)
-# api = Api(app)
-
-# # @app.get('/')
-# # def start(): # https://project-ue.herokuapp.com/
-# #     return ''
-    
-# # @app.get('/prime/<number>')
-# # def check_number(number): # https://project-ue.herokuapp.com/prime/3
-# #     return f'''<h1>{primality_test(escape(number))}</h1>'''
-    
-# # @app.get('/time')
-# # def show_time(): # https://project-ue.herokuapp.com/time
-# #     return "time"
-
-# from flask import Flask
-# from flask_httpauth import HTTPBasicAuth
-# from werkzeug.security import generate_password_hash, check_password_hash
-
-# # app = Flask(__name__)
-# auth = HTTPBasicAuth()
-
-# users = {
-#     "john": generate_password_hash("hello"),
-#     "susan": generate_password_hash("bye")
-# }
-
-# @auth.verify_password
-# def verify_password(username, password):
-#     if username in users and \
-#             check_password_hash(users.get(username), password):
-#         return username
-
-# @app.route('/')
-# @auth.login_required
-# def index():
-#     return "Hello, {}!".format(auth.current_user())
-
-# if __name__ == "__main__":
-#     app.run(debug=False, host="0.0.0.0")
-
-
 from flask import Flask
-from flask_httpauth import HTTPDigestAuth
+from flask_restful import Api
+from markupsafe import escape
+from flask import Flask, render_template, redirect, url_for, request
+
+
+
+from app.primality_test import primality_test
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret key here'
-auth = HTTPDigestAuth()
+# api = Api(app)
+
+# @app.get('/')
+# def start(): # https://project-ue.herokuapp.com/
+#     return ''
+    
+# @app.get('/prime/<number>')
+# def check_number(number): # https://project-ue.herokuapp.com/prime/3
+#     return f'''<h1>{primality_test(escape(number))}</h1>'''
+    
+# @app.get('/time')
+# def show_time(): # https://project-ue.herokuapp.com/time
+#     return "time"
+
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 users = {
-    "john": "hello",
-    "susan": "bye"
+    "john": generate_password_hash("hello"),
+    "susan": generate_password_hash("bye")
 }
 
-@auth.get_password
-def get_pw(username):
-    if username in users:
-        return users.get(username)
-    return None
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
 
 @app.route('/')
 @auth.login_required
 def index():
-    return "Hello, %s!" % auth.username()
+    return "Hello, {}!".format(auth.current_user())
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0")
