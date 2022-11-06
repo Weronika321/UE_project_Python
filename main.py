334fb8ff1788cde6b098df3e5a1287799be1cbd6
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 17:48:46 +0100

    render template

--- main.py
+++ main.py
@@ -47,10 +47,10 @@ def hello_world():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
+    # error = None
+    # if request.method == 'POST':
+    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #         error = 'Invalid Credentials. Please try again.'
+    #     else:
+    #         return redirect(url_for('home'))
+    return render_template('login.html')
