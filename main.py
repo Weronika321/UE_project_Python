26fc5f289124ec33d349103b02e053ac369edc30
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 11:00:28 +0100

    index

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api
 from api.HelloWorld import HelloWorld
 from api.PrimalityTest import PrimalityTest
@@ -13,6 +13,12 @@ api.add_resource(HelloWorld, "/")
 api.add_resource(PrimalityTest, "/prime")
 api.add_resource(InvertColors, "/invert")
 api.add_resource(Time, "/time")
+
+@app.route('/index')
+def access_param():
+    source = request.args.get('source')
+    return '''<h1>The source value is: {}</h1>'''.format(source)
+# appFlask.run(debug=True, port=5000)
 
 
 if __name__ == "__main__":
