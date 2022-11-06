eca1a3e50aff44c051474ae5657a74a6d02fec09
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 12:37:45 +0100

    .

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ def show_time(): # https://project-ue.herokuapp.com/time
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('start'))
+    # if request.method == 'POST':
+    #     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #         error = 'Invalid Credentials. Please try again.'
+    #     else:
+    #         return redirect(url_for('start'))
     return render_template('login.html', error=error)
 
 if __name__ == "__main__":
