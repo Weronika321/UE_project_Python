91043bf906d8c205616de225e36b72ed86e76d33
Weronika Ładak <weronika.ladak1@gmail.com>
2022-11-06 11:28:14 +0100

    clear

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from flask import Flask, request
 from flask_restful import Api
-from api.HelloWorld import HelloWorld
-from api.InvertColors import InvertColors
-from api.Time import Time
+
 from app.primality_test import primality_test
 
 
 app = Flask(__name__)
 api = Api(app)
 
-
-api.add_resource(HelloWorld, "/")
-api.add_resource(InvertColors, "/invert")
-api.add_resource(Time, "/time")
-
+@app.get('/')
+def access_param(): # https://project-ue.herokuapp.com/
+    return ''
+    
 @app.get('/prime')
 def access_param(): # https://project-ue.herokuapp.com/prime?number=3
     number = request.args.get('number')
@@ -21,8 +18,11 @@ def access_param(): # https://project-ue.herokuapp.com/prime?number=3
         return f"Nie podano liczby"
     else:
         return f'''<h1>{primality_test(number)}</h1>'''
-
+    
+@app.get('/time')
+def access_param(): # https://project-ue.herokuapp.com/time
+    return f"time"
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")  # app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
